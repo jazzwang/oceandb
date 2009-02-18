c15c7b056ae1057617f098b8224f03081a2b85c2
jazz <jazz@ec98fd09-b655-494a-921e-9a7ad6c74486>
2009-02-18 03:34:05 +0000

    * 加入 gmap key
    git-svn-id: https://trac.nchc.org.tw/svn/grid/oceandb/jQuery_Prototype@59 ec98fd09-b655-494a-921e-9a7ad6c74486

--- script/oceandb.js
+++ script/oceandb.js
@@ -13,3 +13,14 @@ $(document).ready(function() {
 
   $('#map-menu').accordion({ header: "h3" });
 });
+
+function load() {
+  if (GBrowserIsCompatible()) {
+    var map = new GMap2(document.getElementById("map"));  // 取得 DOM 中,名稱為 map 的元件
+    map.addControl(new GLargeMapControl());               // 加入左上角比例尺規控制列
+    map.addControl(new GScaleControl());                  // 加入左下角比例尺狀態列
+    map.addControl(new GMapTypeControl());                // 加入右上角"地圖","衛星","混合地圖"按鈕
+    map.setCenter(new GLatLng(23.8,121), 7);              // 設定預設經緯度北緯 23.8, 東經 121, 預設比例尺 100 公里(7)
+    map.setMapType(G_SATELLITE_MAP);                      // 設定預設底圖為"衛星"
+  }
+}
