d8679b4c1615ea27ad66a03b16ecef3947966f96
jazz <jazz@ec98fd09-b655-494a-921e-9a7ad6c74486>
2009-02-22 09:21:06 +0000

    * 把本來放在 <body> 中的 load() 改放到 jQuery 的 ready() 函數中
    git-svn-id: https://trac.nchc.org.tw/svn/grid/oceandb/jQuery_Prototype@66 ec98fd09-b655-494a-921e-9a7ad6c74486

--- script/oceandb.js
+++ script/oceandb.js
@@ -2,6 +2,9 @@
 // 參考: http://docs.jquery.com/Events
 $(document).ready(function() {
 
+  // 呼叫 Google Map API 載入地圖
+  load();
+
   var PARENT_ID = [];	  // 產生一個空的陣列
   PARENT_ID[1] = "定點";
   PARENT_ID[2] = "船測";
@@ -74,3 +77,15 @@ $(window).resize(function() {
   var map_height=document.documentElement.clientHeight - 68;
   $('#main').css( { height: map_height } );
 });
+
+function load() 
+{
+  if (GBrowserIsCompatible()) {
+    var map = new GMap2(document.getElementById("map"));  // 取得 DOM 中,名稱為 map 的元件
+    map.addControl(new GLargeMapControl());               // 加入左上角比例尺規控制列
+    map.addControl(new GScaleControl());                  // 加入左下角比例尺狀態列
+    map.addControl(new GMapTypeControl());                // 加入右上角"地圖","衛星","混合地圖"按鈕
+    map.setCenter(new GLatLng(23.8,121), 7);              // 設定預設經緯度北緯 23.8, 東經 121, 預設比例尺 100 公里(7)
+    map.setMapType(G_SATELLITE_MAP);                      // 設定預設底圖為"衛星"
+  }
+}
